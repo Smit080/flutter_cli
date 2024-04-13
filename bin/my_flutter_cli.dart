568c8007c6679448457a034392f9cadd8f7520ef
home <developershouse.80@gmail.com>
2024-04-13 19:01:17 +0530

    first commit

--- bin/my_flutter_cli.dart
+++ bin/my_flutter_cli.dart
@@ -1,4 +1,4 @@
-import 'package:my_code_generator/my_code_generator.dart';
+import 'package:my_flutter_cli/my_flutter_cli.dart';
 
 void main(List<String> arguments) {
   MyFlutterCli().generate(arguments);
